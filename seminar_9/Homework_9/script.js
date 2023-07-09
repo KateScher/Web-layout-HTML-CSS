1556587d42a25600d4b13dcd8fd86ae10cb1f880
Ekaterina Scherbakova <kat-m1@mail.ru>
2023-07-09 22:34:59 +0500

    Add changes hw9

---
+++ seminar_9/Homework_9/script.js
@@ -0,0 +1,10 @@
+const menuActive = document.querySelector(".menu-active"); // Найти меню, console.log(menuActive);
+const headerMenu = document.querySelector(".site-logo"); // найти кнопку бургера
+const menuClose = document.querySelector(".menu-close"); // найти кнопку крестика
+
+function toggleMenu() {
+  menuActive.classList.toggle("hidden"); // ф-я удаляет или устанавливает класс с названием 'hidden" В div с классом .menu-active
+}
+
+headerMenu.addEventListener("click", toggleMenu); // по клику на бургер срабатывает ф-я
+menuClose.addEventListener("click", toggleMenu); // по клику на крестик срабатывает ф-я
